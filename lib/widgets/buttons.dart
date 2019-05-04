07e0f0bf47449187188030a7da645098fbc2dd50
Carrein <addisonho@outlook.com>
2019-05-05 02:58:32 +0800

    Update system font.

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 
+/// Library class encapulsating all button-based widgets.
 class Buttons {
-  Widget textButton(VoidCallback action, String description) {
+  /// Template for standard text button.
+  /// Button take up full width of parent by convention.
+  Widget textButton(VoidCallback action, String text) {
     return Container(
-      padding: EdgeInsets.all(10),
+      padding: EdgeInsets.all(20),
       child: RaisedButton(
         child: SizedBox(
           width: double.infinity,
           child: Text(
-            description,
+            text,
             textAlign: TextAlign.center,
           ),
         ),
         onPressed: () => action,
+      ),
+    );
+  }
+
+  /// Template for standard icon button.
+  /// Button take up full width of parent by convention.
+  Widget iconButton(VoidCallback action, var icon) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.all(20),
+      child: RaisedButton(
+        child: SizedBox(
+          child: Icon(icon),
+        ),
+        onPressed: () => action,
+      ),
+    );
+  }
+
+  /// Template for standard text button.
+  /// Buttons take up full width of parent by convention.
+  Widget switchButton(VoidCallback action, bool state) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Switch(
+        value: state,
+        onChanged: (e) => action,
       ),
     );
   }
