6b172d096038e60ca1ff14b504d309c7a9bcbcdf
Carrein <addisonho@outlook.com>
2019-06-18 08:13:47 +0800

    Refactor Post BLoC naming.

---
+++ lib/feed_bloc/post_event.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+abstract class PostEvent extends Equatable {}
+
+class Fetch extends PostEvent {
+  @override
+  String toString() => 'Fetch';
+}
+
+class Refresh extends PostEvent {
+  @override
+  String toString() => 'Refresh';
+}
