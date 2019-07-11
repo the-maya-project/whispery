70e5bef453d69e6c3e39da4d1175adb31ae6dc5a
Carrein <addisonho@outlook.com>
2019-07-12 00:17:09 +0800

    Refactor tests to ignore main application.

--- test/sample_test.dart
+++ test/sample_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/main.dart';
 
 void main() {
-  testWidgets('app should work', (WidgetTester tester) async {
-    tester.pumpWidget(new App());
+  testWidgets('Placeholder unit test', (WidgetTester tester) async {
   });
 }
